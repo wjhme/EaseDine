5a46aa546487a66ffd86abe7c7e5ba3c56021548
wjhme <1586529701@qq.com>
2025-04-11 01:39:22 +0800

    测试ASR

--- ASR/model_evaluate_cer.py
+++ ASR/model_evaluate_cer.py
@@ -3,7 +3,8 @@ from FunASR.CER_evaluate import calculate_cer
 
 hypothesis = pd.read_csv("/mnt/disk/wjh23/EaseDine/ASR/FunASR/FunASR_train_audio_batch_11.txt",sep="\t") # time: min-0.06;max-6.37;mean-0.07
 hypothesis.columns = ['uuid','pred_text','time']
-hypothesis['pred_text'].fillna("")
+print(hypothesis[hypothesis['pred_text'].isna()])
+hypothesis['pred_text'] = hypothesis['pred_text'].fillna("")
 hypothesis['pred_text'] = hypothesis['pred_text'].str.replace(r'[a-zA-Z\s，。？！]', '', regex=True)
 
 reference = pd.read_csv("/mnt/disk/wjh23/EaseDine/DOM/train.txt",sep="\t")
@@ -20,7 +21,7 @@ text_pairs = list(zip(result_df['text'], result_df['pred_text']))
 
 fun_asr_cer = []
 for ref, hyp in text_pairs:
-    print(ref,hyp)
+    # print(ref,hyp)
     result = calculate_cer(ref, hyp)
     fun_asr_cer.append(result)
 
